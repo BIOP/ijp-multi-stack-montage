245dec69b044df638e3ae39fbcfdfaf8398ac73b
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-09-27 14:37:51 +0200

    fix licenses

--- src/main/java/Multi_Stack_Montage.java
+++ src/main/java/Multi_Stack_Montage.java
@@ -1,3 +1,24 @@
+/*-
+ * #%L
+ * This plugin brings a bit more functionality that was not available using the Make Montage... Plugin, namely making montages out of multiple stacks and hyperstacks.
+ * %%
+ * Copyright (C) 2009 - 2022 BIOP
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.regex.Matcher;
